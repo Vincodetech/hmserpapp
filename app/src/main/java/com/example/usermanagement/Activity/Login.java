b2a16d0dc08f0ba5b49b8478e8d848ed6e2c718e
Vincodetech <71717023+Vincodetech@users.noreply.github.com>
2021-03-15 20:06:19 +0530

    check volley api
    check volley api

--- app/src/main/java/com/example/usermanagement/Activity/Login.java
+++ app/src/main/java/com/example/usermanagement/Activity/Login.java
@@ -1,9 +1,13 @@
 package com.example.usermanagement.Activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,12 +16,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.usermanagement.Constant;
 import com.example.usermanagement.ModelResponse.LoginResponse;
 import com.example.usermanagement.ModelResponse.RegisterResponse;
+import com.example.usermanagement.NavFragment.DashboardFragment;
 import com.example.usermanagement.R;
 import com.example.usermanagement.RetrofitClient;
 import com.example.usermanagement.SharedPrefManager;
+import com.google.gson.JsonObject;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import retrofit2.Call;
@@ -26,9 +47,10 @@ import retrofit2.Response;
 
 public class Login extends AppCompatActivity {
     TextView loginlink;
-    EditText email,password;
+    EditText email, password;
     Button login;
     SharedPrefManager sharedPrefManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +59,10 @@ public class Login extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        loginlink = (TextView)findViewById(R.id.txtGotoRegister);
-        email = (EditText)findViewById(R.id.edtEmail);
-        password = (EditText)findViewById(R.id.edtPassword);
-        login = (Button)findViewById(R.id.btnLogin);
+        loginlink = (TextView) findViewById(R.id.txtGotoRegister);
+        email = (EditText) findViewById(R.id.edtEmail);
+        password = (EditText) findViewById(R.id.edtPassword);
+        login = (Button) findViewById(R.id.btnLogin);
         sharedPrefManager = new SharedPrefManager(getApplicationContext());
         loginlink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,13 +74,138 @@ public class Login extends AppCompatActivity {
         });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                userLogin();
+            public void onClick(View v) {
+                //userLogin();
+                login();
             }
         });
 
     }
+
+    private void login() {
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (userEmail.isEmpty()) {
+            email.requestFocus();
+            email.setError("Please Enter your Email");
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+            email.requestFocus();
+            email.setError("Please Enter Correct Email");
+            return;
+        }
+        if (userPassword.isEmpty()) {
+            password.requestFocus();
+            password.setError("Please Enter your Password");
+            return;
+        }
+        if (userPassword.length() < 6) {
+            password.requestFocus();
+            password.setError("Please Enter your Password to  Maximum 6 Characters");
+            return;
+        }
+
+//        StringRequest request = new StringRequest(Request.Method.POST, Constant.LOGIN, response -> {
+//            JsonObject object = new JsonObject();
+//            if(object.getAsBoolean())
+//            {
+//                JsonObject user = object.getAsJsonObject("users");
+//                SharedPreferences userPref = getApplicationContext().getSharedPreferences("users", Context.MODE_PRIVATE);
+//                SharedPreferences.Editor editor = userPref.edit();
+//                editor.putString("email",user.getAsString());
+//                editor.putString("password",user.getAsString());
+//                editor.apply();
+//                Toast.makeText(Login.this,"Login Success",Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(Login.this, Home.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                startActivity(intent);
+//
+//            }
+//        }, error -> {
+//            error.printStackTrace();
+//        }){
+//            @Nullable
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                HashMap<String,String> map = new HashMap<>();
+//                map.put("email",email.getText().toString().trim());
+//                map.put("password",password.getText().toString());
+//                return map;
+//            }
+//        };
+//
+//        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//        queue.add(request);
+        RequestQueue queue = Volley.newRequestQueue(Login.this);
+        StringRequest request = new StringRequest(Request.Method.POST, Constant.LOGIN,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(Login.this, "Login Success", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Login.this, Home.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("email", email.getText().toString().trim());
+                map.put("password", password.getText().toString());
+                return map;
+
+            }
+        };
+        int socketTimeOut = 10000;
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        request.setRetryPolicy(retryPolicy);
+
+        queue.add(request);
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, Constant.LOGIN,
+//                null, new com.android.volley.Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                Log.e("Response", response.toString());
+//            }
+//        },
+//                new com.android.volley.Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e("Response", error.toString());
+//                    }
+//                }) {
+//            @Nullable
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                HashMap<String,String> map = new HashMap<>();
+//                map.put("email",email.getText().toString().trim());
+//                map.put("password",password.getText().toString());
+//                return map;
+//            }
+//
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                HashMap<String,String> map = new HashMap<>();
+//                map.put("Content-Type","application/x-www-form-urlencoded");
+//                return map;
+//            }
+//        };
+//
+//        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+//        queue.add(request);
+    }
+
+
 
     private void userLogin()
     {
---
+++ app/src/main/java/com/example/usermanagement/Constant.java
@@ -0,0 +1,9 @@
+package com.example.usermanagement;
+
+public class Constant
+{
+    public static final String URL = "http://192.168.42.210/";
+    public static final String HOME = URL + "api";
+    public static final String LOGIN = HOME + "/loginUser";
+    public static final String REGISTER = HOME + "/addUsers";
+}
